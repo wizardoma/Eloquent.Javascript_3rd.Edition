6e889aae0408781843570ecf4519f0ad475f18d9
Ibekason Alexander <alibekason@gmail.com>
2021-03-15 09:57:30 +0100

    feat: Solve minimum exercise in Function

---
+++ 2. Functions/Exercise1-Minimum.js
@@ -0,0 +1,12 @@
+/**
+ * @author Ibekason Alexander Onyebuchi
+ * Date: 15/03/2021
+ */
+
+// Write a function min that takes two arguments and returns their minimum.
+
+const minimum= (a,b) => {
+    return a<b ? a : b;
+}
+
+console.log(minimum(10,24))
